337675c196775aa6266cfaeb95d1606a9c66eb71
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2015-02-09 14:06:04 +0100

    Add maven support

---
+++ src/main/java/com/ragego/engine/package-info.java
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------------------------
+ - RageGo is a Game and GameEngine for Go Game (Jeu de Go)                    -
+ - Copyright (C) 2015  Philippe VIENNE (for code)                             -
+ - Copyright (C) 2015  Benoit RENAULT (for code and graphics)                 -
+ - Copyright (C) 2015  Pierre-Armand NICQ (for graphics)                      -
+ -                                                                            -
+ - This program is free software: you can redistribute it and/or modify       -
+ - it under the terms of the GNU General Public License as published by       -
+ - the Free Software Foundation, either version 3 of the License, or          -
+ - (at your option) any later version.                                        -
+ -                                                                            -
+ - This program is distributed in the hope that it will be useful,            -
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of             -
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              -
+ - GNU General Public License for more details.                               -
+ -                                                                            -
+ - You should have received a copy of the GNU General Public License          -
+ - along with this program.  If not, see <http://www.gnu.org/licenses/>.      -
+ -                                                                            -
+ - The graphical (pictures, assets, logo ...) work is licensed under the      -
+ - Creative Commons Attribution-ShareAlike 3.0 Unported License. To view      -
+ - a copy of this license, visit <http://creativecommons.org/licenses/by-sa/3.0/>
+ - or send a letter to <Creative Commons, PO Box 1866, Mountain View, CA 94042, USA>.
+ -----------------------------------------------------------------------------*/
+
+/**
+ * Created by pvienne on 09/02/15.
+ */
+package com.ragego.engine;
