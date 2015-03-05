d3f90525e0249a483a1fe129df75bb5aa7ec0988
Bene-Gesserit <Bene-Gesserit@users.noreply.github.com>
2015-03-05 14:32:59 +0100

    Pushing test codes
    Former-commit-id: 19558a3ef79a4a74742b05db95a561b374242eb9

--- src/main/java/com/ragego/gui/desktop/Main.java
+++ src/main/java/com/ragego/gui/desktop/Main.java
@@ -1,7 +1,15 @@
 package com.ragego.gui.desktop;
 
-/**
- * Created by pvienne on 09/02/15.
- */
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
 public class Main {
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "hello-world";
+        cfg.width = 480;
+        cfg.height = 320;
+
+        new LwjglApplication(new DisplayTest(), cfg);
+    }
 }
