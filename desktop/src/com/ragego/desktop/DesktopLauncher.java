62035861b6224a93ca1211bc4ee442f7fb221c3c
Bene-Gesserit <Bene-Gesserit@users.noreply.github.com>
2015-04-13 12:21:40 +0200

    First steps in implementing the gui
    Former-commit-id: 91b72d4745dc27b5ed91135d0b999ef7b5420523

--- desktop/src/com/ragego/desktop/DesktopLauncher.java
+++ desktop/src/com/ragego/desktop/DesktopLauncher.java
@@ -2,11 +2,18 @@ package com.ragego.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.ragego.RageGoGame;
+import com.ragego.gui.RageGoGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.height = 600;
+		config.width = 1000;
+		config.resizable = false; //Temporarily blocked for first tests
+
+		config.title = "RageGo";
+
 		new LwjglApplication(new RageGoGame(), config);
 	}
 }
