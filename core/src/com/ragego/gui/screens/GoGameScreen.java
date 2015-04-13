91b72d4745dc27b5ed91135d0b999ef7b5420523
Bene-Gesserit <Bene-Gesserit@users.noreply.github.com>
2015-04-13 12:21:40 +0200

    First steps in implementing the gui

---
+++ core/src/com/ragego/gui/screens/GoGameScreen.java
@@ -0,0 +1,62 @@
+package com.ragego.gui.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+
+public class GoGameScreen extends ScreenAdapter {
+    private TiledMap map;
+    private IsometricTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+
+    @Override
+    public void show() {
+        map = new TmxMapLoader().load("core/res/com/ragego/assets/Goban.tmx");
+        renderer = new IsometricTiledMapRenderer(map);
+        camera = new OrthographicCamera();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.zoom = (float)4;
+        camera.update();
+
+        renderer.setView(camera);
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        renderer.dispose();
+    }
+}
