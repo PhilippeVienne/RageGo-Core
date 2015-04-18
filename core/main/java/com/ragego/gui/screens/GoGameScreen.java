82f0d52202fee932d652779144eaa62ae5173f58
Bene-Gesserit <Bene-Gesserit@users.noreply.github.com>
2015-04-18 12:06:22 +0200

    Implemeted Input detection and conversion into isometric coordinates. I love you Maths.
    Former-commit-id: e3b8f6090ec044be7f598d39a67f6cebfb47d13f

--- core/main/java/com/ragego/gui/screens/GoGameScreen.java
+++ core/main/java/com/ragego/gui/screens/GoGameScreen.java
@@ -1,35 +1,65 @@
 package com.ragego.gui.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.ragego.utils.GuiUtils;
 
 public class GoGameScreen extends ScreenAdapter {
+    private static final String TAG = "GoGameScreen";
+
     private TiledMap map;
+    private float mapUnit;
+    private float yOffset;
     private IsometricTiledMapRenderer renderer;
     private OrthographicCamera camera;
     private ExtendViewport viewport;
+    private Stage stage;
+
+    GestureDetector gesture;
+    InputMultiplexer myInputMultiplexer;
+
+    private float mapWidth, mapHeight;
+
+    TiledMapTileLayer grid_layer;
 
     @Override
     public void show() {
+        /*
+            Map setup
+         */
         map = new TmxMapLoader().load("android/assets/maps/Goban.tmx");
         renderer = new IsometricTiledMapRenderer(map);
         camera = new OrthographicCamera();
+        grid_layer = (TiledMapTileLayer)map.getLayers().get("grid");
 
         //Map size in world units (+1 tile's height to compensate the 3d effect)
-        float mapWidth = (float)map.getProperties().get("width", Integer.class)* map.getProperties().get("tilewidth", Integer.class);
-        float mapHeight = (float)(map.getProperties().get("height", Integer.class) + 1) * map.getProperties().get("tileheight", Integer.class);
+        mapWidth = (float)map.getProperties().get("width", Integer.class)* map.getProperties().get("tilewidth", Integer.class);
+        mapHeight = (float)map.getProperties().get("height", Integer.class) * map.getProperties().get("tileheight", Integer.class);
 
-        //Centers camera on map
-        camera.translate(mapWidth*0.5f, 0);
+        //Map unit (useful for screen/map coordinates conversion)
+        mapUnit = (float)(Math.sqrt(Math.pow(map.getProperties().get("tilewidth", Integer.class) * 0.5d, 2)
+                + Math.pow(map.getProperties().get("tileheight", Integer.class) * 0.5d, 2)));
+
+        //Active Tile Layer Offset on y-axis (TODO make it map.tmx dependent)
+        yOffset = map.getProperties().get("tileheight", Integer.class) * 0.5f;
+
+                //Centers camera on map
+                camera.translate(mapWidth * 0.5f, 0);
 
         //Maximizes the map size on screen
-        viewport = new ExtendViewport(mapWidth, mapHeight, camera);
+        viewport = new ExtendViewport(mapWidth, mapHeight + map.getProperties().get("tileheight", Integer.class), camera);
 
         /*Sets the maps colors (WIP, create a texture color changing method and apply it here.
         Check http://stackoverflow.com/questions/24034352/libgdx-change-color-of-texture-at-runtime for help
@@ -37,11 +67,17 @@ public class GoGameScreen extends ScreenAdapter {
         TiledMapTileSet gridElements = map.getTileSets().getTileSet("gridTS");
         Color gridColor = new Color(Color.BLACK);
 
-        for (int i=0; i<gridElements.; i++) {
+        for (int i=0; i<gridElements.size()-1; i++) {
             gridElements.getTile(i).getTextureRegion().getTexture().getTextureData().
         }
         System.out.println(gridElements.size());
         */
+
+        /*
+            Interaction components setup
+         */
+        gesture = new GestureDetector(new MyGestureListener());
+        Gdx.input.setInputProcessor(gesture);
     }
 
     @Override
@@ -79,5 +115,65 @@ public class GoGameScreen extends ScreenAdapter {
     public void dispose() {
         map.dispose();
         renderer.dispose();
+    }
+    public class MyGestureListener implements GestureDetector.GestureListener {
+
+        @Override
+        public boolean touchDown(float x, float y, int pointer, int button) {
+            Vector3 tempCoords = new Vector3(x,y,0);
+            Vector3 worldCoords = camera.unproject(tempCoords);
+
+            System.out.println("Screen coordinates : "
+                    + "X: " + x + " Y: " + y);
+
+            System.out.println("World coordinates : "
+                    + "X: " + worldCoords.x + " Y: " + worldCoords.y);
+
+            Vector2 isoCoords = GuiUtils.worldToIso(worldCoords, mapUnit, yOffset);
+
+            System.out.println("Isometric coordinates : "
+                    + "X: " + isoCoords.x + " Y: " + isoCoords.y);
+
+            System.out.println("******************************");
+
+            return false;
+        }
+
+        @Override
+        public boolean tap(float x, float y, int count, int button) {
+            return false;
+        }
+
+        @Override
+        public boolean longPress(float x, float y) {
+            return false;
+        }
+
+        @Override
+        public boolean fling(float velocityX, float velocityY, int button) {
+            return false;
+        }
+
+        @Override
+        public boolean pan(float x, float y, float deltaX, float deltaY) {
+            return false;
+        }
+
+        @Override
+        public boolean panStop(float x, float y, int pointer, int button) {
+            //Gdx.app.log("Text", "panstop");
+            return false;
+        }
+
+
+        @Override
+        public boolean zoom (float originalDistance, float currentDistance){
+            return false;
+        }
+
+        @Override
+        public boolean pinch (Vector2 initialFirstPointer, Vector2 initialSecondPointer, Vector2 firstPointer, Vector2 secondPointer){
+            return false;
+        }
     }
 }
---
+++ core/main/java/com/ragego/utils/GuiUtils.java
@@ -0,0 +1,50 @@
+package com.ragego.utils;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class GuiUtils {
+    // Constants (useful for screen/map coordinates conversion)
+    private static final double PI_OVER_SIX = Math.PI/6;
+    private static final double PI_OVER_THREE = 2*PI_OVER_SIX;
+
+    /**
+     * Projects world coordinates into isometric coordinates through the solution of a triangle.
+     *
+     * @param worldCoords The position in world coordinates
+     * @return The coordinate corresponding to the position in the isometric Coordinate system
+     */
+    public static Vector2 worldToIso (Vector3 worldCoords, float mapUnit, float yOffset){
+        //Adjust the position of the map's origin
+        float y = worldCoords.y - yOffset;
+        float x = worldCoords.x;
+
+        System.out.println("World coordinates adjusted : "
+                + "X: " + x + " Y: " + y);
+
+        // Computes triangulation values : side of the triangle and two angles
+        double vectorNorm = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        double alpha = PI_OVER_SIX - Math.atan(y/x);
+        double beta = PI_OVER_THREE - alpha;
+
+        System.out.println("Triangulation values : "
+                + "vectorNorm: " + vectorNorm + " beta: " + beta + " alpha: " + alpha);
+
+        // Projects the world coordinates on the isometric coordinate systems's axes
+        Vector2 isoCoords = new Vector2((float)(vectorNorm*Math.sin(alpha)/Math.sin(alpha+beta)),
+                (float)(vectorNorm*Math.sin(beta)/Math.sin(alpha+beta)));
+
+        System.out.println("Iso coordinates projected: "
+                + "X: " + isoCoords.x + " Y: " + isoCoords.y);
+
+        //Norms the projections and casts them
+        isoCoords.x = (int)(isoCoords.x/mapUnit);
+        isoCoords.y = (int)(isoCoords.y/mapUnit);
+
+        return isoCoords;
+    }
+
+    public Vector2 isoToWorld (Vector2 isoCoords){
+        return null;
+    }
+}
