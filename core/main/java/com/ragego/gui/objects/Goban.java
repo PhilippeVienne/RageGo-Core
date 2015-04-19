767d85335d4b93e667605218915ae9ca5927bf1b
Bene-Gesserit <Bene-Gesserit@users.noreply.github.com>
2015-04-19 11:57:44 +0200

    Fixed Goban constructor.
    Former-commit-id: e4ff4313d29cd1685e5db6dbf0102968af1ce01a

--- core/main/java/com/ragego/gui/objects/Goban.java
+++ core/main/java/com/ragego/gui/objects/Goban.java
@@ -7,32 +7,27 @@ import com.ragego.engine.GameBoard;
 import com.ragego.engine.HumanPlayer;
 import com.ragego.gui.screens.GoGameScreen;
 
-import java.awt.*;
-
 public class Goban {
     private final GoGameScreen screen;
     private final TiledMap map;
     private final TiledMapTileLayer gridLayer;
-    private final Vector2 gobanOriginCoords = new Vector2(0, 0);
+    private final Vector2 gobanOriginCoords;
     private final int gobanSize;
     private final GameBoard board;
 
-    public Goban(GoGameScreen screen, TiledMap map, HumanPlayer humanPlayer, HumanPlayer player) {
+    public Goban(GoGameScreen screen, TiledMap map, HumanPlayer player1, HumanPlayer player2) {
         this.screen = screen;
         this.map = screen.getMap();
         this.gridLayer = (TiledMapTileLayer) map.getLayers().get("grid");
-        for (int x = 0; x < gridLayer.getWidth(); x++) {
-            for (int y = 0; y < gridLayer.getHeight(); y++) {
-                if (gridLayer.getCell(x, y).getTile().getProperties().get("name").equals("interTop")){
-                    gobanOriginCoords.set(x, y);
-                }
-            }
-        }
-        this.board = new GameBoard(humanPlayer, player);
-        this.gobanSize = board.getBoardSize();
+
+        gobanOriginCoords = new Vector2(Float.parseFloat(map.getProperties().get("goOriginX", String.class)),
+                Float.parseFloat(map.getProperties().get("goOriginY", String.class)));
+
+        this.gobanSize = Integer.parseInt(map.getProperties().get("gobanSize", String.class));
+        board = new GameBoard(player1, player2, gobanSize);
     }
 
-    public Point isoToGoban (Point isoCoords){
+    public Vector2 isoToGoban (Vector2 isoCoords){
         //If isoCoords.x too small or too big, or if isoCoords.y too small or too big
         return null;
     }
