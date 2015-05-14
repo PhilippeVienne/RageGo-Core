25c4041803a9742786426fcee1846cd866665e02
Bene-Gesserit <Bene-Gesserit@users.noreply.github.com>
2015-05-14 18:34:30 +0200

    Finally correctly understood viewports, this is awesome !

--- core/main/java/com/ragego/gui/screens/GuiTestScreen.java
+++ core/main/java/com/ragego/gui/screens/GuiTestScreen.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.ragego.gui.RageGoGame;
@@ -16,11 +20,21 @@ import com.ragego.gui.RageGoGame;
  */
 public class GuiTestScreen extends ScreenAdapter {
     private static final String TAG = "MenuScreen";
+
+    private static final int SCENE_WIDTH = 1280;
+    private static final int SCENE_HEIGHT = 720;
+
     protected AssetManager manager;
-    private Viewport viewport;
-    private Stage stage;
+    private Viewport backViewport;
+    private ScreenViewport hudViewport;
+    private Stage backStage;
     private Skin skin;
-    private TextButton button1;
+    private Dialog yesNoWindow;
+    private Image backGroundImg;
+    private Texture backTex;
+    private OrthographicCamera backCamera;
+    private OrthographicCamera hudCamera;
+    private Stage hudStage;
 
     public GuiTestScreen() {
         super();
@@ -28,32 +42,74 @@ public class GuiTestScreen extends ScreenAdapter {
 
     @Override
     public void show() {
-        viewport = new ScreenViewport();
-        stage = new Stage(viewport);
+        backCamera = new OrthographicCamera();
+        hudCamera = new OrthographicCamera();
+        backViewport = new FillViewport(2048, 1380, backCamera);
+        hudViewport = new ScreenViewport(hudCamera);
+        hudViewport.setUnitsPerPixel(2);
+
+        backStage = new Stage(backViewport);
+        hudStage = new Stage(hudViewport);
 
         manager = RageGoGame.getAssetManager();
         manager.load("com/ragego/gui/skins/ui_gray.json", Skin.class);
+        manager.load("com/ragego/gui/splash/island_background.png", Texture.class);
         manager.finishLoading();
         Gdx.app.log(TAG, "Assets loaded");
 
         skin = manager.get("com/ragego/gui/skins/ui_gray.json");
 
-        button1 = new TextButton("SkinDefaultButton", skin);
+        backTex = manager.get("com/ragego/gui/splash/island_background.png");
+        backTex.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        Table layerBackground = new Table();
+        backGroundImg = new Image(backTex);
+        layerBackground.add(backGroundImg);
+        backStage.addActor(backGroundImg);
 
-        stage.addActor(button1);
-        //stage.setDebugAll(true);
-        Gdx.input.setInputProcessor(stage);
+        //Window
+        Label message = new Label("Dialog: Exit?", skin);
+        TextButton tb1 = new TextButton("Yes", skin);
+        tb1.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log(TAG, "Yes button clicked");
+                RageGoGame.goHome();
+            }
+        });
+        TextButton tb2 = new TextButton("No", skin);
+        tb2.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log(TAG, "No button clicked");
+            }
+        });
+
+        yesNoWindow = new Dialog("", skin);
+        yesNoWindow.getContentTable().row().colspan(1).center();
+        yesNoWindow.getContentTable().add(message);
+        yesNoWindow.row().colspan(2);
+        yesNoWindow.button(tb1);
+        yesNoWindow.button(tb2);
+        yesNoWindow.setKeepWithinStage(true);
+        yesNoWindow.pack();
+        yesNoWindow.setPosition(hudViewport.getWorldWidth() - yesNoWindow.getWidth() * 0.5f,
+                hudViewport.getWorldHeight() - yesNoWindow.getHeight() * 0.5f);
+        hudStage.addActor(yesNoWindow);
+
+        Gdx.input.setInputProcessor(hudStage);
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        backViewport.update(width, height);
+        hudViewport.update(width, height, true);
     }
 
     @Override
     public void dispose() {
         skin.dispose();
-        stage.dispose();
+        hudStage.dispose();
+        backStage.dispose();
     }
 
     @Override
@@ -61,8 +117,11 @@ public class GuiTestScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f));
-        stage.draw();
+        backStage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f));
+        backStage.draw();
+
+        hudStage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 60f));
+        hudStage.draw();
     }
 
     @Override
